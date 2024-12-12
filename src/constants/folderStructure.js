0b2f8a6015db566fa75a0e99c66b1b81994b308c
jmuteru <jeffmuteru@gmail.com>
2024-12-12 06:51:11 +0300

    v1.1.0

--- src/constants/folderStructure.js
+++ src/constants/folderStructure.js
@@ -1,34 +1,33 @@
-export  const folderStructure = {
+export const folderStructure = {
   // Page router...
-    pageRouter: [
-      'pages/api',            // API routes
-      'pages/components',     // Components for easy routing and layout management
-      'components/common',    // Reusable common components
-      'components/layout',    // Layout components
-      'styles',               // Global styles
-      'public',               // Static files (images, fonts)
-      'hooks',                // Custom React hooks
-      'contexts',             // React context for global state
-      'services',             // API calls and services
-      'store',                // Store for state management (Redux / Zustand..)
-      'types',                // TypeScript types
-      'tests',                // Unit and integration tests
-    ],
+  pageRouter: [
+    'pages/api',            // API routes
+    'pages/components',     // Components for easy routing and layout management
+    'components/common',    // Reusable common components
+    'components/layout',    // Layout components
+    'styles',               // Global styles
+    'public',               // Static files (images, fonts)
+    'hooks',                // Custom React hooks
+    'contexts',             // React context for global state
+    'services',             // API calls and services
+    'store',                // Store for state management (Redux / Zustand..)
+    'types',                // TypeScript types
+    'tests',                // Unit and integration tests
+  ],
 
-    // App router...
-    appRouter: [
-      'app/api',              // API routes for app router
-      'app/components',       // Layout components
-      'components/common',    // Reusable common components
-      'components/layout',    // Layout components
-      'styles',               // Global styles
-      'public',               // Static files (images, fonts)
-      'hooks',                // Custom React hooks
-      'contexts',             // React context for global state
-      'services',             // API calls and services
-      'store',                // Store for state management (Redux / Zustand..)
-      'types',                // TypeScript types
-      'tests',                // Unit and integration tests
-    ],
-  };
-  
+  // App router...
+  appRouter: [
+    'app/api',              // API routes for app router
+    'app/components',       // Layout components
+    'components/common',    // Reusable common components
+    'components/layout',    // Layout components
+    'styles',               // Global styles
+    'public',               // Static files (images, fonts)
+    'hooks',                // Custom React hooks
+    'contexts',             // React context for global state
+    'services',             // API calls and services
+    'store',                // Store for state management (Redux / Zustand..)
+    'types',                // TypeScript types
+    'tests',                // Unit and integration tests
+  ],
+};
